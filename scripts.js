d00cb48656a15c696a67ae9e34ce31ab8f6d23aa
Coutin-05 <120342150+Coutin-05@users.noreply.github.com>
2024-01-14 16:55:47 -0300

    Add functions in scripts.js
    playRound
    getPlayerChoice
    game

--- scripts.js
+++ scripts.js
@@ -1,11 +1,41 @@
-function getComputerChoice() {
+function getComputerChoice() { //generate a random number up to 3 and assign it to one of the options, rock, paper or scissors
     let randomNumber = Math.floor(Math.random()*3) +1;
     if (randomNumber === 1) {
-        console.log("paper");
+        return "paper";
     } else if (randomNumber === 2) {
-        console.log("rock");
+        return "rock";
     } else {
-        console.log("scissors")
+        return "scissors";
     }
 }
-getComputerChoice();
+
+function playRound (playerSelection, computerSelection) { //determine the winner and return message if player win or lose
+    if (playerSelection == computerSelection) {
+        return "It's a tie!";
+    } else if ( //player win
+        (playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "paper" && computerSelection == "rock") ||
+        (playerSelection == "scissors" && computerSelection == "paper")
+    ) {
+        return `You Win! ${playerSelection} beats ${computerSelection}`
+    } else { //player lose
+        return `You Lose! ${computerSelection} beats ${playerSelection}`
+    }
+
+}
+
+function getPlayerChoice() { //get the player input and converts to lowercase
+    let choice = prompt("Rock Paper Scissors");
+    let choiceLowerCase = choice.toLowerCase();
+    return choiceLowerCase;
+}
+
+function game() { //run the playRound function 5 times and log it's result
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = getPlayerChoice ();
+        let computerSelection = getComputerChoice ();
+        console.log(playRound(playerSelection,computerSelection));
+    }
+}
+game()
+
